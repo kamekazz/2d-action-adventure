1f558370af9a10c45fc2680740eb5f20613aae8c
kamekazz <taveras78@gmail.com>
2026-01-30 22:36:05 -0500

    variable Scope n export

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Player ready with move_speed: ", move_speed)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_vector * move_speed
+	move_and_slide()
