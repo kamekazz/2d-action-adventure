41ce7e443f7ce01bc6c41fd3036a902195b6ff8e
kamekazz <taveras78@gmail.com>
2026-01-31 17:18:49 -0500

    donte move

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,25 +2,24 @@ extends CharacterBody2D
 
 @export var move_speed: float = 100
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Player ready with move_speed: ", move_speed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var move_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = move_vector * move_speed
 
-	if velocity.x > 0:
-		$AnimatedSprite2D.play("move_right")
-	elif velocity.y < 0:
-		$AnimatedSprite2D.play("move_up")
-
-	elif velocity.y > 0:
-		$AnimatedSprite2D.play("move_down")
-
-	elif velocity.x < 0:
-		$AnimatedSprite2D.play("move_left")
+	if velocity == Vector2.ZERO:
+		$AnimatedSprite2D.stop()
+	elif abs(velocity.x) > abs(velocity.y):
+		if velocity.x > 0:
+			$AnimatedSprite2D.play("move_right")
+		else:
+			$AnimatedSprite2D.play("move_left")
 	else:
-		$AnimatedSprite2D.play("idle")
+		if velocity.y > 0:
+			$AnimatedSprite2D.play("move_down")
+		else:
+			$AnimatedSprite2D.play("move_up")
+
 	move_and_slide()
