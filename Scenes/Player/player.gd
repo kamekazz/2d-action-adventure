3dfe73e7cfa97ed4710882025eef3df6dced4baa
kamekazz <taveras78@gmail.com>
2026-02-01 20:01:48 -0500

    pushing the block

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var move_speed: float = 100
+@export var push_strength: float = 25
 
 func _ready() -> void:
 	position = SceneManaager.player_spawn_position
@@ -22,5 +23,14 @@ func _physics_process(delta: float) -> void:
 			$AnimatedSprite2D.play("move_down")
 		else:
 			$AnimatedSprite2D.play("move_up")
-
 	move_and_slide()
+	
+	var collision: KinematicCollision2D = get_last_slide_collision()
+	if collision:
+		var collider_node = collision.get_collider()
+		
+		if collider_node is RigidBody2D:
+			var collision_normal: Vector2 = collision.get_normal()
+			collision_normal = collision_normal * push_strength
+			collider_node.apply_central_force(-collision_normal)
+	
